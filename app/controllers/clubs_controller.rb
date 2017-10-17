18a54cb09a8ac8ba87e15e7af2cab4e4ff0fbc8c
Sinan Şahin <bote.sinansahin@gmail.com>
2017-10-17 09:56:36 +0300

    üye olunan kulüpler listesi sorgusunu güncelle

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -9,8 +9,8 @@ class ClubsController < ApplicationController
       @clubs = Club.search(params[:search]).order('name ASC')
     else
       @clubs = Club.order('name ASC').includes(:club_setting, :club_category)
-      @all_members_count = ClubPeriod.all_members_count_by_club_period(@clubs.map(&:id))
-      @clubs_of_current_user = current_user.present? && current_user.active_club_periods ? current_user.active_club_periods.compact.map { |club_period| club_period.club if club_period.present? } : []
+      club_ids_of_current_user = current_user.present? && current_user.roles ? current_user.roles.map(&:club_id).compact : []
+      @clubs_of_current_user = Club.where(id: club_ids_of_current_user)
     end
 
     # excel dökümü için sorgulama
