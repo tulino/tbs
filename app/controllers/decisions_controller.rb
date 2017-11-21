b25170d9fdb5ffde4751032550c5f53dcbbd4bfd
tulino <tulin.ozen@bil.omu.edu.tr>
2017-11-21 14:39:08 +0300

    CRUD işlemlerini yap

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,13 +1,55 @@
 class DecisionsController < ApplicationController
-  def new
-  end
+  
+  before_action :set_decision, only: %i[show edit update destroy]
 
   def index
+    if params[:search]
+      @decisions = Decision.search(params[:search]).order('name ASC')
+    else
+      @decisions = Decision.order('name ASC')
+    end
   end
 
-  def show
+  def new
+    @decision = Decision.new
+    authorize @decision
   end
 
-  def edit
+  def create
+    authorize Decision
+    @decision = Decision.create(decision_params)
+    if @decision.save
+      redirect_to @decision
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    authorize @decision
+    if @decision.update(decision_params)
+      redirect_to @decision
+    else
+      render 'edit',alert: "kayıt basarısız" 
+    end
+  end
+
+  def destroy
+    @decision.destroy
+    redirect_to @decision
+  end
+
+  private
+
+  def decision_params
+    params.require(:decision).permit(:name, :file)
+  end
+
+  def set_decision
+    @decision = Decision.find(params[:id])
   end
 end
