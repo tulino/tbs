77a444dd1936e2a397400b9f1ca2eb70aaffd139
tulino <tulin.ozen@bil.omu.edu.tr>
2017-11-21 14:48:37 +0300

    validation tanımla,arama methodu ekle

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,2 +1,10 @@
 class Decision < ActiveRecord::Base
+  validates :name, presence: true
+  validates :file, presence: true
+  
+  mount_uploader :file, FileUploader
+
+  def self.search(query)
+    where('lower(name) like ?', "%#{query}%".downcase)
+  end
 end
